4e1d3240fb46d51d1a82c8310bf58f63e2aaf121
Ranu <O@O.net>
2016-10-19 16:36:40 +0100

    Add IModificationHistory to the domain

--- NinjaDomain.Classes/Enums/EquipmentType.cs
+++ NinjaDomain.Classes/Enums/EquipmentType.cs
@@ -1,9 +1,14 @@
-﻿namespace NinjaDomain.Classes.Enums
+﻿using System.ComponentModel;
+
+namespace NinjaDomain.Classes.Enums
 {
     public enum EquipmentType
     {
+        [Description("Tool")]
         Tool = 1,
+        [Description("Weapon")]
         Weapon = 2,
-        Outwear = 3
+        [Description("Outerwear")]
+        Outerwear = 3
     }
 }
--- NinjaDomain.Classes/Enums/NinjaType.cs
+++ NinjaDomain.Classes/Enums/NinjaType.cs
@@ -4,6 +4,5 @@
     {
         Shinobi = 1,
         Kunoichi = 2
-
     }
 }
